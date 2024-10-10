c1737b04cfe5d15ea9d5536130453c7150d44546
Jessica Mulein <jessica@digitaldefiance.org>
2024-10-10 19:13:30 +0000

    add coverage

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,19 @@ module.exports = {
     '@react-native-async-storage/async-storage':
       '<rootDir>/__mocks__/@react-native-async-storage/async-storage.js',
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/**/*.{ts,tsx}', // Include all .ts and .tsx files
+    '!<rootDir>/**/*.d.ts', // Exclude TypeScript declaration files
+    '!<rootDir>/**/*test.{ts,tsx}', // Exclude test files
+    '!<rootDir>/dist/**', // Exclude dist folder
+    '!<rootDir>/build/**', // Exclude build folder
+    '!<rootDir>/node_modules/**', // Exclude node_modules folder
+    '!<rootDir>/coverage/**', // Exclude coverage folder
+    '!<rootDir>/.expo/**', // Exclude .expo folder
+    '!<rootDir>/devcontainer/**', // Exclude devcontainer folder
+    '!<rootDir>/.vscode/**', // Exclude .vscode folder
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
 };
